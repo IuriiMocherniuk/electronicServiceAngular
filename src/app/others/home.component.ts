7954ea90025957f324b3d23057392855cadd4041
Iurii Mocherniuk <iurii.mocherniuk@gmail.com>
2021-10-30 21:15:57 +0300

    small refactoring after DEMO

--- src/app/others/home.component.ts
+++ src/app/others/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: `<h1>This is home page!</h1>`
+  template: `<h1>Electronic Service App</h1>`
 })
 export class HomeComponent{
 
